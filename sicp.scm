1a51a8f874eae327c9b534ac9cefa97d0e4e498c
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-02-13 19:45:54 +0300

    sqrt

--- sicp.scm
+++ sicp.scm
@@ -1,3 +1,27 @@
+(define (p) (p))
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
+(define (square x)
+  (* x x))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+		 x)))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+
 (define (fib n)
   (cond ((< n 2) n)
 	(else (+ (fib (- n 1))
