b6bf8e9157119bed9f0422d9deef36ab7e0e358b
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-02-19 20:37:50 +0300

    mult

--- sicp.scm
+++ sicp.scm
@@ -81,3 +81,20 @@
 	 (fast-expt-iter (* a b) b (- n 1)))))
 (define (fast-expti b n)
   (fast-expt-iter 1 b n))
+
+(define (mult a b)
+  (if (= b 0)
+      0
+      (+ a (mult a (- b 1)))))
+
+(define (double x)
+  (* x 2))
+(define (halve x)
+  (/ x 2))
+(define (fast-mult a b)
+  (cond ((= b 0)
+	 0)
+	((even? b)
+	 (fast-mult (double a) (halve b)))
+	(else
+	 (+ a (fast-mult a (- b 1))))))
