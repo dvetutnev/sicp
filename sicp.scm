8173d3ba321d5c213979444649713796e634ffa9
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-02-19 20:31:01 +0300

    fast-expt-iter

--- sicp.scm
+++ sicp.scm
@@ -71,3 +71,13 @@
   (cond ((= n 0) 1)
 	((even? n) (square (fast-expt b (/ n 2))))
 	(else (* b (fast-expt b (- n 1))))))
+
+(define (fast-expt-iter a b n)
+  (cond ((= n 0)
+	 a)
+	((even? n)
+	 (fast-expt-iter a (* b b) (/ n 2)))
+	(else
+	 (fast-expt-iter (* a b) b (- n 1)))))
+(define (fast-expti b n)
+  (fast-expt-iter 1 b n))
