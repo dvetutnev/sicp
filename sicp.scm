c84ad9fa906b80ad496ecb1b88f44f946c94d4ad
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-02-22 01:33:47 +0300

    find-divisor

--- sicp.scm
+++ sicp.scm
@@ -111,3 +111,18 @@
   (if (= b 0)
       a
       (gcd b (remainder a b))))
+
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+	((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (+ test-divisor 1)))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
