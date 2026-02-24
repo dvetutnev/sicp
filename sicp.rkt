6e912be5e1c89436b4021f056dab5e81219ff4fa
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-02-24 23:52:01 +0300

    optimization find-fevisor

--- sicp.rkt
+++ sicp.rkt
@@ -117,9 +117,13 @@
   (= (remainder b a) 0))
 
 (define (find-divisor n test-divisor)
+  (define (next n)
+    (if (= n 2)
+	3
+	(+ n 2)))
   (cond ((> (square test-divisor) n) n)
 	((divides? test-divisor n) test-divisor)
-	(else (find-divisor n (+ test-divisor 1)))))
+	(else (find-divisor n (next test-divisor)))))
 
 (define (smallest-divisor n)
   (find-divisor n 2))
@@ -151,3 +155,4 @@
 	    (else
 	     (timed-prime-test start)
 	     (search-for-primes (+ start 2) end)))))
+	
