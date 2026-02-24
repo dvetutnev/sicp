0332c9ce79a7945e05c58e3c23a460ec44a3b03e
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-02-24 20:26:48 +0300

    search-for-primes

--- sicp.rkt
+++ sicp.rkt
@@ -126,3 +126,28 @@
 
 (define (prime? n)
   (= n (smallest-divisor n)))
+
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (current-inexact-milliseconds) start-time))
+      null))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (current-inexact-milliseconds)))
+
+(define (search-for-primes start end)
+  (if (even? start)
+      (search-for-primes (+ start 1) end)
+      (cond ((> start end)
+	     (newline)
+	     (display "done"))
+	    (else
+	     (timed-prime-test start)
+	     (search-for-primes (+ start 2) end)))))
