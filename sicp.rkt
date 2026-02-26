62dfb78ddadab02ab17643a674afed00d1cbfc91
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-02-26 21:57:34 +0300

    integral-simpson

--- sicp.rkt
+++ sicp.rkt
@@ -181,3 +181,12 @@
   (define (add-dx x) (+ x dx))
   (* (sum f (+ a (/ dx 2)) add-dx b)
      dx))
+
+(define (integral-simpson f a b n)
+  (define h (/ (- b a) n))
+  (define (add-2h x) (+ x h h))
+  (* (+ (f a)
+	(* 2 (sum f (add-2h a) add-2h (- b h)))
+	(* 4 (sum f (+ a h) add-2h b))
+	(f b))
+     (/ h 3)))
