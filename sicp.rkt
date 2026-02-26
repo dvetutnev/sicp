68213400215874260e8d849668ec5c1d09585b59
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-02-27 00:41:47 +0300

    producti/factoriak

--- sicp.rkt
+++ sicp.rkt
@@ -180,7 +180,17 @@
 	(iter (next x) (+ result (term x)))))
   (iter a 0))
 
+(define (producti term a next b)
+  (define (iter x result)
+    (if (> x b)
+	result
+	(iter (next x) (* result (term x)))))
+  (iter a 1))
+
 (define (identity x) x)
+
+(define (factorial n)
+  (producti identity 1 inc n))
 
 (define (inc x) (+ x 1))
 (define (cube x) (* x x x))
