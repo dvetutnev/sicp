477bd71dcd14d94f7820401976ac5c891ff7f716
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-03-10 19:26:16 +0300

    smooth

--- sicp.rkt
+++ sicp.rkt
@@ -343,3 +343,12 @@
   (if (= n 1)
       f
       (compose f (repeated f (- n 1)))))
+
+(define (smooth f)
+  (lambda (x) (/ (+ (f (- x dx))
+		    (f x)
+		    (f (+ x dx)))
+		 3)))
+
+(define (smooth-nth f n)
+  ((repeated smooth n) f))
