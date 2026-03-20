3506d316aa340c5c19cc71e9d9c91263bebadd79
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-03-21 00:11:10 +0300

    coin change

--- sicp2.rkt
+++ sicp2.rkt
@@ -56,3 +56,20 @@
   (if (null? lst)
       lst
       (append (reversec (cdr lst)) (list (car lst)))))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+          (+ (cc amount
+                 (except-first-denomination coin-values))
+              (cc (- amount
+                     (first-denomination coin-values))
+                  coin-values)))))
+
+(define (no-more? coins) (null? coins))
+(define (except-first-denomination coins) (cdr coins))
+(define (first-denomination coins) (car coins))
+
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
