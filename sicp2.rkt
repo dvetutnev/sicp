f8a2747ea4a6e881b3e7154632b873089890f544
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-04-04 23:18:08 +0300

    fold

--- sicp2.rkt
+++ sicp2.rkt
@@ -283,3 +283,14 @@
 
 (define (dot-product v w)
   (accumulate + 0 (map * v w)))
+
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+	result
+	(iter (op result (car rest))
+	      (cdr rest))))
+  (iter initial sequence))
+
+(define fold-right accumulate)
