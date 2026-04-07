582efe174cca77155ce98cfdf6c37ba893638d89
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-04-08 01:44:00 +0300

    prime?

--- sicp2.rkt
+++ sicp2.rkt
@@ -308,3 +308,25 @@
 
 (define (reverser sequence)
   (fold-right (lambda (x y) (append y (list x))) null sequence))
+
+
+(define (square n)
+  (* n n))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (find-divisor n test-divisor)
+  (define (next n)
+    (if (= n 2)
+	3
+	(+ n 2)))
+  (cond ((> (square test-divisor) n) n)
+	((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (next test-divisor)))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
