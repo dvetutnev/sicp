ca61496e261da60234931c34d3e6e9cc6170eca3
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-04-19 00:43:08 +0300

    union-set

--- sicp2.rkt
+++ sicp2.rkt
@@ -546,3 +546,11 @@
 	 (cons (car set1)
 	       (intersection-set (cdr set1) set2)))
 	(else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+	((null? set2) set1)
+	((element-of-set? (car set1) set2)
+	 (union-set (cdr set1) set2))
+	(else (cons (car set1)
+		    (union-set (cdr set1) set2)))))
