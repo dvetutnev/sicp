0b4c84cc35d8240faeb3a8ff693a29858478a761
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-04-19 00:29:36 +0300

    intersection-set

--- sicp2.rkt
+++ sicp2.rkt
@@ -539,3 +539,10 @@
   (if (element-of-set? x set)
       set
       (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+	((element-of-set? (car set1) set2)
+	 (cons (car set1)
+	       (intersection-set (cdr set1) set2)))
+	(else (intersection-set (cdr set1) set2))))
