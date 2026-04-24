f209a4405fccb15955e4c7f974fa892d4540e233
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-04-24 22:35:51 +0300

    tree->list-1

--- sicp2.63.rkt
+++ sicp2.63.rkt
@@ -35,3 +35,10 @@
 	 (make-tree (entry set)
 		    (left-branch set)
 		    (adjoin-set x (right-branch set))))))
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+	      (cons (entry tree)
+		    (tree->list-1 (right-branch tree))))))
