86259c778f4617fc060e885683bcded694580efb
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-04-28 23:52:34 +0300

    make-leaf-set

--- sicp2.67.rkt
+++ sicp2.67.rkt
@@ -73,3 +73,19 @@
 	((element-of-set? symbol (symbols (right-branch tree)))
 	 (cons 1 (encode-symbol symbol (right-branch tree))))
 	(else (error "symbol not in tree - ENCODE-SYMBOL"))))
+
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+	((< (weight x) (weight (car set)))
+	 (cons x set))
+	(else (cons (car set)
+		    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+	(adjoin-set (make-leaf (car pair)
+			       (cadr pair))
+		    (make-leaf-set (cdr pairs))))))
