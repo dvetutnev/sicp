313aa3c74434780305ea8a05f2c5ade05cc6eec0
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-06-13 23:35:46 +0300

    euler-transform

--- sicp3streams.rkt
+++ sicp3streams.rkt
@@ -84,3 +84,14 @@
 
 (define pi-stream
   (scale-stream (partial-sum (pi-summands 1)) 4))
+
+(define (square x)
+  (* x x))
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))
+	(s1 (stream-ref s 1))
+	(s2 (stream-ref s 2)))
+    (stream-cons (- s2 (/ (square (- s2 s1))
+			  (+ s0 (* -2 s1) s2)))
+		 (euler-transform (stream-rest s)))))
