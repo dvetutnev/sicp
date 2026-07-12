26e45de1c8fd6b5d8bab167bfaa2525f323f1af3
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-07-12 23:05:44 +0300

    RC

--- sicp3streams.rkt
+++ sicp3streams.rkt
@@ -221,3 +221,13 @@
 		 (add-streams (scale-stream integrated dt)
 			      int)))
   int)
+
+(define (RC R C dt)
+  (lambda (i v0)
+    (add-streams (scale-stream i R)
+		 (integral (scale-stream i (/ 1 C))
+			   v0
+			   dt))))
+
+(define RC1 (RC 5 1 0.5))
+
