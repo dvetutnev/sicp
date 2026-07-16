3f33e973aca8dd1e7376fe1dbf5797200b4343fc
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2026-07-16 18:15:33 +0300

    zero-crossings

--- sicp3streams.rkt
+++ sicp3streams.rkt
@@ -242,3 +242,15 @@
 (define sence-data
   (list->stream
    (list 1 2 1.5 1 0.5 -0.1 -2 -3 -2 -0.5 0.2 3 4 0 0 0 0 0 -1 0 0 0 0 1 0 0)))
+
+(define (sign-change-detector current previous)
+  (cond ((and (>= current 0)
+	      (< previous 0)) 1)
+	((and (< current 0)
+	      (>= previous 0)) -1)
+	(else 0)))
+
+(define zero-crossings
+  (stream-mapc sign-change-detector
+	       sence-data
+	       (stream-cons 0 sence-data)))
